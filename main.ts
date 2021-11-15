e49c8cf1dc98f2e6e72f7964cbb900b3c8db62bb
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2021-11-14 23:24:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (input.temperature() > -10 && input.temperature() < 0) {
-        music.playTone(196, music.beat(BeatFraction.Whole))
+        music.ringTone(262)
     } else if (input.temperature() > 0 && input.temperature() < 10) {
-        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.ringTone(392)
     } else if (input.temperature() > 10 && input.temperature() < 20) {
-        music.playTone(494, music.beat(BeatFraction.Whole))
+        music.ringTone(494)
     } else if (input.temperature() > 20 && input.temperature() < 30) {
-        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.ringTone(988)
     }
 })
